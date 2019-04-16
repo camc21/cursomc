80f0990dd809d0d833d643ae1c15899c5835e324
Guto <gutosjc1@gmail.com>
2019-04-16 20:19:04 -0300

    Criado insert de Categoria

--- src/main/java/br/com/camc/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/camc/cursomc/resources/CategoriaResource.java
@@ -1,14 +1,19 @@
 package br.com.camc.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.camc.cursomc.domain.Categoria;
 import br.com.camc.cursomc.services.CategoriaService;
+import ch.qos.logback.core.net.server.ServerListener;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -21,6 +26,14 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Long id) {
 		Categoria obj = categoriaService.buscar(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = categoriaService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getid()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }

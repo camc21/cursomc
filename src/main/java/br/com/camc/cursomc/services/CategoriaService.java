c7e7359d755da8d4f2bbfc8b74b74ad6e5864c8a
Guto <gutosjc1@gmail.com>
2019-04-21 15:24:49 -0300

    realizado paginação com parametros opcionais na requisição.

--- src/main/java/br/com/camc/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/camc/cursomc/services/CategoriaService.java
@@ -5,8 +5,10 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
-
 import br.com.camc.cursomc.domain.Categoria;
 import br.com.camc.cursomc.repositories.CategoriaRepository;
 import br.com.camc.cursomc.services.exceptions.DataIntegrityException;
@@ -48,5 +50,10 @@ public class CategoriaService {
 	
 	public List<Categoria>findAll(){
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage,Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
